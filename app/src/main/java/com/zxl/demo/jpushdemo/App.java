026722d357165c4074b2aa4466e2d30964ccf5e4
zxl <zxiaolongo@163.com>
2019-08-30 17:41:46 +0800

    别名设置成功 Tag设置后后台发送无设备

--- app/src/main/java/com/zxl/demo/jpushdemo/App.java
+++ app/src/main/java/com/zxl/demo/jpushdemo/App.java
@@ -2,6 +2,9 @@ package com.zxl.demo.jpushdemo;
 
 import android.app.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import cn.jpush.android.api.JPushInterface;
 
 public class App extends Application {
@@ -10,5 +13,16 @@ public class App extends Application {
         super.onCreate();
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
+
+        //notice 设置别名，可以成功
+        JPushInterface.setAlias(this,100,"123456");
+
+        //notice 设置Tag发送失败
+//        Set<String> set = new HashSet<>();
+//        set.add("aaa");
+//        set.add("bbb");
+//        JPushInterface.setTags(this,100,set);
+        String registrationID = JPushInterface.getRegistrationID(this);
+
     }
 }
