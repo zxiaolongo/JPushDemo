0b42bc82d5d720db62b436619c88beafd94b1518
zxl <zxiaolongo@163.com>
2019-08-30 17:22:18 +0800

    极光推送demo

--- app/src/main/java/com/zxl/demo/jpushdemo/MainActivity.java
+++ app/src/main/java/com/zxl/demo/jpushdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zxl.demo.jpushdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zxl.demo.jpushdemo.service.MyJCommonService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //启动后开启服务，否则关闭app收不到通知（原因未知）
+        startService(new Intent(this,MyJCommonService.class));
     }
 }
